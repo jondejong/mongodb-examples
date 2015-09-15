391943b7bb844f9a06524d4db7d0e067cf606147
Jon DeJong <jon.dejong@gmail.com>
2015-09-14 21:03:07 -0500

    adding update

--- ui/src/app/components/add-dog/add-dog.controller.js
+++ ui/src/app/components/add-dog/add-dog.controller.js
@@ -1,13 +1,13 @@
 'use strict';
 
 angular.module('ui')
-    .controller('AddDogCtrl', function ($http, $state) {
+    .controller('AddDogCtrl', function ($http, $state, dogService) {
       var self = this;
 
       self.dog = {};
 
       self.save = function() {
-        $http.post('http://localhost:4000/dogs', self.dog).then(function() {
+        dogService.save(self.dog).then(function() {
           $state.go('home.dogs');
         });
       }
--- ui/src/app/components/dogs-sevice/dog.service.js
+++ ui/src/app/components/dogs-sevice/dog.service.js
@@ -15,6 +15,24 @@ angular
           return $http.delete('http://localhost:4000/dogs/' + dog._id, dog).then(function (response) {
             return response.data;
           });
+        },
+
+        save: function(dog) {
+          return $http.post('http://localhost:4000/dogs', dog).then(function(response) {
+            return response.data;
+          });
+        },
+
+        update: function(dog) {
+          return $http.put('http://localhost:4000/dogs/' + dog._id, dog).then(function(response) {
+            return response.data;
+          });
+        },
+
+        get: function(id) {
+          return $http.get('http://localhost:4000/dogs/' + id).then(function(response) {
+            return response.data;
+          });
         }
       };
 
