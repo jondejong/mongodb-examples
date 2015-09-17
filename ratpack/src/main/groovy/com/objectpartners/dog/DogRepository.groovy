722e89fe54eaa99443c2677f35857628b4ace110
Jon DeJong <jon.dejong@gmail.com>
2015-09-17 16:50:34 -0500

    making dogRepository a Service

--- ratpack/src/main/groovy/com/objectpartners/dog/DogRepository.groovy
+++ ratpack/src/main/groovy/com/objectpartners/dog/DogRepository.groovy
@@ -1,20 +1,33 @@
 package com.objectpartners.dog
 
 import com.gmongo.GMongo
-import com.google.inject.Inject
+import com.google.inject.Singleton
 import ratpack.exec.Blocking
+import ratpack.server.Service
+import ratpack.server.StartEvent
+import ratpack.server.StopEvent
 
 
 /**
  * Created by jondejong on 9/16/15.
  */
-class DogRepository {
+@Singleton
+class DogRepository implements Service {
 
+    GMongo mongo
     def database
 
-    @Inject
-    DogRepository(){
-        database = new GMongo("127.0.0.1", 27017).getDB('doggies')
+    @Override
+    void onStart(StartEvent event) throws Exception {
+        println "Opening MongoDB Connection"
+        mongo = new GMongo("127.0.0.1", 27017)
+        database = mongo.getDB('doggies')
+    }
+
+    @Override
+    void onStop(StopEvent event) throws Exception {
+        println "Closing MongoDB Connection"
+        mongo.close()
     }
 
     def getDogs() {
